e883681b094d983d72e1f4012184eb58477d8d8d
camillegdn <145430933+camillegdn@users.noreply.github.com>
2023-09-22 11:42:34 +0200

    Add files via upload

---
+++ Boucle/script.js
@@ -0,0 +1,14 @@
+"use a strict"
+/* 
+Annoncer une variable a qui vaut un chiffre
+Annoncer une variable i =0
+Tant que i n'a pas atteint a alors il ajoute 1
+Afficher -> console.log(" C'est carré")
+
+*/
+
+let a=4
+for (let i = 0; i < a; i++) {
+    console.log("C'est carré");
+    
+}
